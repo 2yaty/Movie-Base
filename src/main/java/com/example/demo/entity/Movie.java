b1ba5996ba5ca630898650e43c43c2b40039bde5
Mohamed Ali Qiaty <eng.ayaty@gmail.com>
2025-02-18 20:09:16 +0200

    feat: adding entities (User, Movie, Rating)

---
+++ src/main/java/com/example/demo/entity/Movie.java
@@ -0,0 +1,27 @@
+package com.example.demo.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "movies")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    private String year;
+    private String genre;
+    private String director;
+    private String plot;
+    private String posterUrl;
+}
