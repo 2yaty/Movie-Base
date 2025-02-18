596287e74894a7a2f0be188efdef4950639d28ce
Mohamed Ali Qiaty <eng.ayaty@gmail.com>
2025-02-18 20:53:05 +0200

    feat: adding user repo and services

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,24 @@
+package com.example.demo.service;
+
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User registerUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword())); // Hash password
+        return userRepository.save(user);
+    }
+}
